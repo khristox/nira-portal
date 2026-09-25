a6e7c692335e00645d59bc21110d4b84086fc96b
unknown <UEDCL@UEDCL-ICT26LP08.ad.uedcl.co.ug>
2026-09-25 19:54:49 +0300

    Add GitHub Actions for Docker build and push

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "standalone",
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
 };
 
 export default nextConfig;
